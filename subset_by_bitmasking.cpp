b512df91eb5cb9af6c59fe2f0462f2b89a7168d3
Niloy Chandra Deb <138240550+Niloyyy@users.noreply.github.com>
2023-10-08 03:25:14 +0600

    Update subset_by_bitmasking.cpp

--- subset_by_bitmasking.cpp
+++ subset_by_bitmasking.cpp
@@ -8,13 +8,10 @@ int main(){
     cin.tie(NULL);
     int n; cin >> n;
     vector<int>v(n);
-    int ss = 0;
-	for(int i = 0 ; i < n;i++) cin >> v[i];
+    for(int i = 0 ; i < n;i++) cin >> v[i];
     for(int i = 0 ; i < (1 << n);i++){
-    	int sum = 0;
     	for(int j = 0 ; j < n;j++){
-    		if(i &(1 << j))
-    		cout << v[j]<<" ";
+    		if(i &(1 << j)) cout << v[j]<<" ";
     	}
     	cout <<endl;
     }
